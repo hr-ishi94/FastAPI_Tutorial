e3c559221afd851d5c2ae25e97fc90c5db13af79
hprasad <hrishikeshprasadc@gmail.com>
2024-10-06 12:22:40 +0530

    query parameters

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blog list'}
+@app.get('/blog')                         #queryparameters  ===>   #http://localhost:8000/blog?limit=50&unpublished=true 
+def index(limit = 10,published:bool= False): #will also be able to assign default values and also datatypes for the query parameters
+    if published:
+        return {'data':f'{limit} published blog list'}
+    else:
+        return {'data':'No unpublished found'}
 
 @app.get('/blog/unpublished/') #if same route is given for both dynamic and static function always add static route above all the same dynamic ones as to avoid errors
 def unpublished():
